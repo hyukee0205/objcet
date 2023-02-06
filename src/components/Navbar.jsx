fe4fc56c0679d92289b3e9d33f5d88752c821eee
hyukee0205 <hyukee0205@gmail.com>
2023-02-06 18:21:37 +0900

    0206

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import { BsFillPencilFill } from 'react-icons/bs';
+import { login } from '../api/firebase';
+
+
+
+export default function Navbar() {
+  return (
+    <header className='flex justify-between border-b border-gray-300 p-2'>
+      <Link to='/' className='flex itmes-cneter text-4xl text-brand'>
+        <FiShoppingBag />
+        <h1>Object</h1>
+      </Link>
+      <nav className='flex items-center gap-4 font-semibold'>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>Carts</Link>
+        <Link to='/products/new' className='text-2xl'>
+          <BsFillPencilFill />
+        </Link>
+        <button onClick={login}>Login</button>
+      </nav>
+    </header>
+  )
+}
---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AllProducts() {
+  return (
+    <div>
+      All Products
+    </div>
+  )
+}
