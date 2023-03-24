c8f523524f07217270676ad39763314b644abf55
전찬혁 <hyukee0205@gmail.com>
2023-03-24 20:28:00 +0900

    0324

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getProducts } from '../api/firebase';
 import ProductCard from './ProductCard';
+import useProducts from '../hooks/useProducts';
 
 export default function Products() {
   const {
-    isLoading,
-    error,
-    data: products,
-  } = useQuery(['products'], getProducts);
+    productsQuery: {isLoading, error, data: products}
+  } = useProducts();
   return (
     <>
       <h2 className='px-24 pt-6 text-3xl font-medium'>NEW ARRIVAL</h2>
