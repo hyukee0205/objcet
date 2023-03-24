1c4ff1fe962e257b2a1f96d91a435f84291a1194
전찬혁 <hyukee0205@gmail.com>
2023-03-25 02:17:20 +0900

    banner 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,8 +6,8 @@ export default function Banner() {
     <section className='h-[32rem] bg-yellow-900 relative'>
       <div className='w-full bg-cover h-full bg-banner opacity-80' />
       <div className='absolute w-full top-40 text-center text-gray-50 drop-shadow-2xl'>
-        <h2 className='text-6xl mb-4'>React Project !</h2>
-        <p className='text-2xl'>thank you for coming</p>
+        <h2 className='text-6xl mb-4'>Select Shop !</h2>
+        <p className='text-2xl'>Thank you for coming</p>
       </div>
     </section>
   );
